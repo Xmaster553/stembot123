9a6fc2e271222744571f3fde88761b933decf81e
Xmaster553 <85641905+Xmaster553@users.noreply.github.com>
2021-06-09 21:29:43 +0300

    Add files via upload

---
+++ stem.py
@@ -0,0 +1,152 @@
+import discord
+import os
+import asyncio
+import datetime
+from discord.ext import commands
+from discord.ext import commands, tasks
+from discord.utils import get
+from discord.ext.commands import Bot
+import sys
+import random
+
+from config import settings
+
+nd = datetime.datetime.now()
+
+client = commands.Bot(command_prefix = settings["PREFIX"], intents=discord.Intents(
+        guilds=True, members=True, messages=True, reactions=True, presences=True, voice_states=True
+    ))
+client.remove_command( 'help' )
+
+@client.event
+async def on_ready():
+	now_time = nd.strftime("%H:%M:%S") 
+	print(">> Консоль <<" " Бот запущен "+(now_time)) 
+	await client.change_presence( status = discord.Status.online, activity=discord.Game ("Discord") )
+	printer.start()
+
+@tasks.loop(minutes=12)
+async def printer():
+	channel = client.get_channel(848248409806602301)
+	nds = datetime.datetime.now()
+	now_time = nds.strftime(" %H:%M:%S ")
+	await channel.send(f"Привет!")
+
+@client.event
+async def on_member_join(member):
+	channel = client.get_channel(848248409806602301)
+
+	emb = discord.Embed(title = 'ДОБРО ПОЖАЛОВАТЬ', description = f'**{member.name}** приветствуем тебя на сервере {member.guild.name}. На нашем сервере мы проводим ивенты, общаемся и играем в разные игры.\n\n**ОЗНАКОМЬСЯ С ДАННЫМИ КАНАЛАМИ:**', color = 0xCC974F)
+	emb.add_field(
+        name = '<#825464795784151111>',
+        value = 'Ознакомься с правилами сервера.', inline = False)
+	emb.add_field(
+        name = '<#825466544141238303>',
+        value = 'список команд для взаимодействия с ботами.', inline = False)
+	emb.add_field(
+        name = '<#825694586441695262>',
+        value = 'проходят ежедневные розыгрыши с хорошими призами.', inline = False)
+	emb.set_footer(text="ПРИЯТНОГО ВРЕМЯ ПРОВЕДЕНИЯ НА НАШЕМ СЕРВЕРЕ")
+
+	role_1 = member.guild.get_role(835554688559611961)
+	await member.add_roles(role_1)
+	await channel.send(embed = emb)
+
+Bad_word = ["бан","кик"] 
+
+@client.event 
+async def on_message(message): 
+	await client.process_commands(message) 
+	mes = message.content.lower()
+	avtorMsg = message.author.name 
+	channelMassage = message.channel.id
+	channelMassage2 = message.channel.name
+	now_datetime = datetime.datetime.now() 
+	now_times = nd.strftime(" = %H : %M : %S = ") 
+	date_dm = nd.strftime(" - %d.%m.%y %b -") 
+	print(format(now_datetime) + " > " + str(channelMassage) + "-" + channelMassage2 + " > " + avtorMsg + " > " + mes) 
+	with open("Massage.txt", "a", encoding = "utf-8") as logmsg: 
+		logmsg.write(f"{date_dm} >>> {now_times} >> {channelMassage}-{channelMassage2} > {avtorMsg} = {mes}\n") 
+	for i in Bad_word: 
+		if i in mes:
+			await message.delete()
+			msg = await message.channel.send(f"Не надо такие страсти говорить")
+			with open("Bad.txt", "a", encoding = "utf-8") as logmsg:
+				logmsg.write(f"{date_dm} >>> {now_times} >> {channelMassage}-{channelMassage2} > {avtorMsg} = {mes}\n")
+			await asyncio.sleep(8)
+			await msg.delete()
+
+@client.event
+async def on_command_error(ctx, err):
+    if isinstance(err, commands.CommandNotFound):
+        Err1 = await ctx.send(embed=discord.Embed(description=f"Команда не найдена!"))
+        await asyncio.sleep(6)
+        await Err1.delete()
+
+    elif isinstance(err, commands.BotMissingPermissions):
+        Err2 = await ctx.send(
+            embed=discord.Embed(description=f"У бота отсутствуют права: {' '.join(err.missing_perms)}\nВыдайте их ему для полного функционирования бота"))
+        await asyncio.sleep(6)
+        await Err2.delete()
+
+    elif isinstance(err, commands.MissingPermissions):
+        Err3 = await ctx.send(embed=discord.Embed(description=f"У вас недостаточно прав для запуска этой команды!"))
+        await asyncio.sleep(6)
+        await Err3.delete()
+
+    elif isinstance(err, commands.UserInputError):
+        Err4 = await ctx.send(embed=discord.Embed(description=f"Правильное использование команды {ctx.command}({ctx.command.brief}): `{ctx.command.usage}`"))
+        await asyncio.sleep(6)
+        await Err4.delete()
+
+    elif isinstance(err, commands.CommandOnCooldown):
+        Err5 = await ctx.send(embed=discord.Embed(description=f"У вас еще не прошел кулдаун на команду {ctx.command}!\nПодождите еще {err.retry_after:.2f}"))
+        await asyncio.sleep(6)
+        await Err5.delete()
+
+    elif isinstance(err, dpy_errors.Forbidden):
+        Err6 = await ctx.send(embed=discord.Embed(description=f"У бота нет прав на запуск этой команды!"))
+        await asyncio.sleep(6)
+        await Err6.delete()
+    else:
+        Err7 = await ctx.send(embed=discord.Embed(description=f"Произошла неизвестная ошибка: `{err}`\nПожалуйста, свяжитесь с разработчиками для исправления этой ошибки"))
+        await asyncio.sleep(6)
+        await Err7.delete()
+
+@client.command(aliases = ["лог", "log"])
+@commands.cooldown(1, 24, commands.BucketType.user)
+@commands.has_any_role("админ")
+async def __log(ctx):
+	Masg = await ctx.send(file = discord.File(fp = "Massage.txt"))
+	await asyncio.sleep(12)
+	await Masg.delete()
+
+@client.command(aliases = ["clear","очистка", "cl"])
+@commands.cooldown(1, 8, commands.BucketType.user)
+@commands.has_any_role("Модератор", "админ")
+async def __clear(ctx, amount = 1):
+	await ctx.channel.purge(limit = 1)
+	await asyncio.sleep(1)
+	await ctx.channel.purge(limit = amount)
+	Mas = await ctx.send(f':thumbsup: Удалено {amount} сообщений')
+	await asyncio.sleep(4)
+	await Mas.delete()
+
+@client.command(aliases = ["ping","пинг"])
+@commands.cooldown(1, 8, commands.BucketType.user)
+async def __ping(ctx):
+	await ctx.send(f"понг!")
+
+@client.command(aliases = ["reload", "перезагрузка"])
+
+@commands.has_any_role("админ")
+async def __reload(ctx):
+	embed = discord.Embed(description = "**БОТ ПЕРЕЗАПУСКАЕТСЯ**", color = 0xf5ce42)
+	embedmas = await ctx.send(embed=embed)
+	await asyncio.sleep(2)
+	await embedmas.delete()
+	await os.execv(sys.executable, ["python"] + sys.argv)
+
+token = os.environ.get('token')
+
+client.run(token)
