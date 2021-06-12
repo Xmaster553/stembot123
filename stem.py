4599bc247ed400ebb8f8b259557f0f296adea8fe
Xmaster553 <85641905+Xmaster553@users.noreply.github.com>
2021-06-12 17:48:16 +0300

    Update stem.py

--- stem.py
+++ stem.py
@@ -124,12 +124,14 @@ async def on_voice_state_update(member, before, after):
 		for guild in client.guilds:
 			maincategory = discord.utils.get(guild.categories, id=825730948715905076)
 			channel2 = await guild.create_voice_channel(name=f'Канал {member.display_name}', category = maincategory)
+			print(f'Канал {channel2} создан!')
 			await channel2.set_permissions(member,connect=True,manage_channels=True)
 			await member.move_to(channel2)
 			def check(x,y,z):
 				return len(channel2.members) == 0
 			await client.wait_for('voice_state_update',check=check)
 			await channel2.delete()
+			print(f'Канал {channel2} удален!')
 
 #-------------------------------------------------------------------------------------------------
 #@client.command(aliases = ["лог", "log"])
