fa5c22e9fc3da622ccbc45793504f7a66e2316c8
Xmaster553 <85641905+Xmaster553@users.noreply.github.com>
2021-06-15 22:04:21 +0300

    Update stem.py

--- stem.py
+++ stem.py
@@ -127,7 +127,7 @@ async def __reload(ctx):
 	
 @client.command(aliases = ["mute", "мут"])
 @commands.has_permissions(view_audit_log=True)
-async def __mute(ctx,member:discord.Member,*,time:int,reason):
+async def __mute(ctx,member:discord.Member,*,time = int,reason):
 	muterole = discord.utils.get(ctx.guild.roles, id=825804010271145984)
 	emb = discord.Embed(title=f'ВЫ ПОЛУЧИЛИ МЬЮТ НА' + time + 'ПО ПРИЧИНЕ' + reason, color = 0xf5ce42)
 	await member.add_roles(muterole)
