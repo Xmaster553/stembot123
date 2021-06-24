c527b9294bb76393594f81c9fc7c409d15ca9fc7
Xmaster553 <85641905+Xmaster553@users.noreply.github.com>
2021-06-24 19:00:02 +0300

    Update stem.py

--- stem.py
+++ stem.py
@@ -120,6 +120,25 @@ async def on_raw_reaction_add(payload):
             except Exception as e:
                 print(repr(e))
 		
+@client.event
+async def on_raw_reaction_remove(payload):
+        channel = get_channel(payload.channel_id) # получаем объект канала
+        message = await channel.fetch_message(payload.message_id) # получаем объект сообщения
+        member = get(message.guild.members, id=payload.user_id) # получаем объект пользователя который поставил реакцию
+ 
+        try:
+            emoji = str(payload.emoji) # эмоджик который выбрал юзер
+            role = get(message.guild.roles, id=config.ROLES[emoji]) # объект выбранной роли (если есть)
+ 
+            await member.remove_roles(role)
+            print('[SUCCESS] Role {1.name} has been remove for user {0.display_name}'.format(member, role))
+ 
+        except KeyError as e:
+            print('[ERROR] KeyError, no role found for ' + emoji)
+        except Exception as e:
+            print(repr(e))
+
+		
 #-------------------------------------------------------------------------------------------------
 #@client.command(aliases = ["лог", "log"])
 #@commands.cooldown(1, 24, commands.BucketType.user)
