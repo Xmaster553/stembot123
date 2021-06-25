1820a77bec3deff24bffa33306e734cb67b70950
Xmaster553 <85641905+Xmaster553@users.noreply.github.com>
2021-06-25 22:00:29 +0300

    Update stem.py

--- stem.py
+++ stem.py
@@ -61,7 +61,7 @@ async def on_member_join(member):
 
 @client.event 
 async def on_message(message): 
-    pass
+    print(message)
 	
 @client.event
 async def on_voice_state_update(member, before, after):
