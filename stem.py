9b603248b1b32e8eea27233aece83bc161ab5226
Xmaster553 <85641905+Xmaster553@users.noreply.github.com>
2021-06-28 10:32:31 +0300

    Update stem.py

--- stem.py
+++ stem.py
@@ -40,13 +40,13 @@ async def on_member_join(member):
 	emb = discord.Embed(title = 'ДОБРО ПОЖАЛОВАТЬ', description = f'**{member.name}** приветствуем тебя на сервере {member.guild.name}. На нашем сервере мы проводим ивенты, общаемся и играем в разные игры.\n\n**ОЗНАКОМЬСЯ С ДАННЫМИ КАНАЛАМИ:**', color = 0xCC974F)
 	emb.add_field(
         name = f':arrow_forward: <#825464795784151111>',
-        value = 'Ознакомься с правилами сервера.', inline = False, url = https://discord.gg/KFpePpT3YA)
+        value = 'Ознакомься с правилами сервера.', inline = False)
 	emb.add_field(
         name = f':arrow_forward: <#825466544141238303>',
-        value = 'список команд для взаимодействия с ботами.', inline = False, url = https://discord.gg/yQAau4vK2W)
+        value = 'список команд для взаимодействия с ботами.', inline = False)
 	emb.add_field(
         name = f':arrow_forward: <#825694586441695262>',
-        value = 'проходят ежедневные розыгрыши с хорошими призами.', inline = False, url = https://discord.gg/6MXrueUd63)
+        value = 'проходят ежедневные розыгрыши с хорошими призами.', inline = False)
 	emb.set_footer(text="ПРИЯТНОГО ВРЕМЯ ПРОВЕДЕНИЯ НА НАШЕМ СЕРВЕРЕ")
 
 	role_1 = member.guild.get_role(838788077547028571)
