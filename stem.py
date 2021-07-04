6ec6b937320f9c88741418bdac54eaedc13b6cd0
Xmaster553 <85641905+Xmaster553@users.noreply.github.com>
2021-07-04 21:08:33 +0300

    Update stem.py

--- stem.py
+++ stem.py
@@ -154,7 +154,26 @@ async def __reload(ctx):
 	
 @client.command(aliases = ["mute", "мут"])
 @commands.has_permissions(view_audit_log=True)
-async def __mute(ctx,member:discord.Member,time:int,*,reason):
+async def __mute(ctx,member:discord.Member,time:str,*,reason):
+	total = 0
+	number = 0
+	for i in string:
+		if i in ['0', '1', '2',  '3', '4', '5', '6', '7', '8', '9']:
+			number = int(str(number) + str(i))
+		else:
+			if i == 'd':
+				total = total + number * 24 * 60 * 60
+				number = 0
+			elif i == 'h':
+				total = total + number * 60 * 60
+				number = 0
+			elif i == 'm':
+				total = total + number * 60
+				number = 0
+			elif i == 's':
+				total = total + number
+				number = 0
+		return total
 	muterole = discord.utils.get(ctx.guild.roles, id=825804010271145984)
 	emb = discord.Embed(title=f'ВЫ ПОЛУЧИЛИ МЬЮТ НА {time} секунд ПО ПРИЧИНЕ {reason}', color = 0xf5ce42)
 	await member.add_roles(muterole)
