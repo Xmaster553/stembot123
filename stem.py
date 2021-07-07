edb69741e341258b84070b93dec1c74e968afebb
Xmaster553 <85641905+Xmaster553@users.noreply.github.com>
2021-07-07 13:49:37 +0300

    Update stem.py

--- stem.py
+++ stem.py
@@ -151,7 +151,7 @@ async def __mute(ctx,member:discord.Member,time:str,*,reason=None):
     
 	if member.id == ctx.author.id:
 		await ctx.send(f"{ctx.author.mention}, ты не можешь **Muted** себя!")
-			pass
+	pass
 		
 	muterole = discord.utils.get(ctx.guild.roles, id=825804010271145984)
 	emb = discord.Embed(title=f'ВЫ ПОЛУЧИЛИ МЬЮТ НА {time} секунд ПО ПРИЧИНЕ {reason}', color = 0xf5ce42)
