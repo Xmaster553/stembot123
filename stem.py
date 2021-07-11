75f4d80279c6b8652e9ed544ec2a240f700127bb
Xmaster553 <85641905+Xmaster553@users.noreply.github.com>
2021-07-11 11:56:23 +0300

    Update stem.py

--- stem.py
+++ stem.py
@@ -145,7 +145,7 @@ async def __reload(ctx):
 	
 @client.command(aliases = ["mute", "мут"])
 @commands.has_permissions(view_audit_log=True)
-async def __mute(ctx,member:discord.Member,time:int,*,reason=None):
+async def __mute(ctx,member:discord.Member,time=0,*,reason=None):
 	if reason == None:
 		return await ctx.send("Ошибка\n`.mute [name] [time] [reason]`")
 	
